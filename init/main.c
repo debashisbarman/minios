01ebb42c3b5da5fa393754bbb5b7c4d290077158
Debashis Barman <debashisb04@gmail.com>
2016-03-05 00:57:52 +0530

    Mini OS 0.0.01

--- init/main.c
+++ init/main.c
@@ -1,21 +1,3 @@
-/*
- * Mini Operating System 0.0.01
- * Copyright (C) 2016 Debashis Barman (http://debashisbarman.in)
- *
- * This program is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation; either version 2 of the License, or
- * (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program; if not, write to the Free Software
- * Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
- */
 #define __MINIOS_LIBC 1
 #include <stddef.h>
 #include <stdarg.h>
